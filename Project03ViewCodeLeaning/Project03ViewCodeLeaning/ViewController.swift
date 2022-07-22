3bb9045367da193ee802b7d8c92fe3f785103519
Carlos Philipe Mendes Bahia <carlosphbahia@gmail.com>
2022-07-22 16:04:01 -0300

    feat: Cria label por viewcodeWq

--- Project03ViewCodeLeaning/Project03ViewCodeLeaning/ViewController.swift
+++ Project03ViewCodeLeaning/Project03ViewCodeLeaning/ViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Carlos"
+        label.font = UIFont.boldSystemFont(ofSize: 35)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.backgroundColor = .blue
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        super.view.backgroundColor = .white
+        view.addSubview(self.nameLabel)
+        self.setConstraints()
+    }
+
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            self.nameLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.nameLabel.centerYAnchor.constraint(
+                equalTo: self.view.centerYAnchor
+            ),
+            self.nameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+        ])
     }
 
 
