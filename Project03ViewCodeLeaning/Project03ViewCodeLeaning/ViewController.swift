00fee6092d83bfd21cfd6eaed3fff0d2d7716fd6
Carlos Philipe Mendes Bahia <carlosphbahia@gmail.com>
2022-07-22 20:54:37 -0300

    feat: Criação de switch

--- Project03ViewCodeLeaning/Project03ViewCodeLeaning/ViewController.swift
+++ Project03ViewCodeLeaning/Project03ViewCodeLeaning/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     lazy var nameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -20,21 +19,44 @@ class ViewController: UIViewController {
         return label
     }()
 
+    lazy var checkSwitch: UISwitch = {
+        let s = UISwitch()
+        s.translatesAutoresizingMaskIntoConstraints = false
+        s.isOn = true
+        s.onTintColor = .blue
+        s.addTarget(self, action: #selector(self.tappedSiwtch(send:)), for: .touchUpInside)
+        
+        return s
+    }()
+
+    @objc func tappedSiwtch(send: UISwitch) {
+        print("Disparou state: \(send.isOn)")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         super.view.backgroundColor = .white
         view.addSubview(self.nameLabel)
+        view.addSubview(self.checkSwitch)
+
         self.setConstraints()
     }
 
     private func setConstraints() {
         NSLayoutConstraint.activate([
             self.nameLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            self.nameLabel.centerYAnchor.constraint(
-                equalTo: self.view.centerYAnchor
+            self.nameLabel.topAnchor.constraint(
+                equalTo: self.view.topAnchor, constant: 64
             ),
             self.nameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+        ])
+
+        NSLayoutConstraint.activate([
+            self.checkSwitch.centerXAnchor.constraint(equalTo: self.nameLabel.centerXAnchor),
+            self.checkSwitch.topAnchor.constraint(
+                equalTo: self.nameLabel.bottomAnchor, constant: 16
+            ),
         ])
     }
 
